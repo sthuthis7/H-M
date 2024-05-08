910a3e92a9c29c531a59f365cd8d94a885504bfd
sthuthis7 <sthuthi.s@hm.com>
2024-05-08 12:22:53 +0530

    Updated with Interface

--- ProjectJava1.java
+++ ProjectJava1.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
+interface Company{
+    void pf();
+    void cab();
+}
+
 abstract class Emp {
     int id;
     String name;
     int age;
     float salary;
     String designation;
-
+    float pf;
+    boolean cab=false;
     Emp() {
 
         Scanner sc = new Scanner(System.in);
@@ -28,12 +34,14 @@ abstract class Emp {
         System.out.println("Salary:\t\t" + salary);
         System.out.println("Age:\t\t" + age);
         System.out.println("Designation:\t" + designation);
+        System.out.println("PF:\t\t"+pf);
+        System.out.println("Cab:\t\t"+cab);
     }
 
     abstract void raiseSalary(); //Abstraction is implemented!!
 }
 
-final class Dev extends Emp { //making the class Final so that no other class can access and modify the Dev class
+final class Dev extends Emp implements Company { //making the class Final so that no other class can access and modify the Dev class
     Dev() {
         designation = "Developer";
         salary = 1200000;
@@ -45,9 +53,21 @@ final class Dev extends Emp { //making the class Final so that no other class ca
             salary=(float)(salary*1.1);
         
     }
+    @Override
+    public void pf(){
+        System.out.println("---------------------------------------------------\n-------------!PF has been updated!-------------");
+        if (pf==0){pf=(float)(salary*0.1);}
+        else{
+            pf=(float)(pf+salary*0.1);
+        }
+    }
+    @Override
+    public void cab(){
+        cab=true;
+    }
 }
 
-final class Clerk extends Emp { //making the class Final so that no other class can access and modify the Clerk class
+final class Clerk extends Emp implements Company{ //making the class Final so that no other class can access and modify the Clerk class
     Clerk() {
         designation = "Clerk";
         salary = 600000;
@@ -59,9 +79,21 @@ final class Clerk extends Emp { //making the class Final so that no other class 
         salary=(float)(salary*1.1);
     
     }   
+    @Override
+    public void pf(){
+        System.out.println("---------------------------------------------------\n-------------!PF has been updated!-------------");
+        if (pf==0){pf=(float)(salary*0.1);}
+        else{
+            pf=(float)(pf+salary*0.1);
+        }
+    }
+    @Override
+    public void cab(){
+        cab=true;
+    }
 }
 
-final class Manager extends Emp { //making the class Final so that no other class can access and modify the Manager class
+final class Manager extends Emp  implements Company{ //making the class Final so that no other class can access and modify the Manager class
     Manager() {
         designation = "Manager";
         salary = 1800000;
@@ -73,9 +105,21 @@ final class Manager extends Emp { //making the class Final so that no other clas
         salary=(float)(salary*1.1);
     
     }  
+    @Override
+    public void pf(){
+        System.out.println("---------------------------------------------------\n-------------!PF has been updated!-------------");
+        if (pf==0){pf=(float)(salary*0.1);}
+        else{
+            pf=(float)(pf+salary*0.1);
+        }
+    }
+    @Override
+    public void cab(){
+        cab=true;
+    }
 }
 
-final class Tester extends Emp { //making the class Final so that no other class can access and modify the Tester class
+final class Tester extends Emp  implements Company{ //making the class Final so that no other class can access and modify the Tester class
     Tester() {
         designation = "Tester";
         salary = 700000;
@@ -87,6 +131,18 @@ final class Tester extends Emp { //making the class Final so that no other class
         salary=(float)(salary*1.1);
     
     }  
+    @Override
+    public void pf(){
+        System.out.println("---------------------------------------------------\n-------------!PF has been updated!-------------");
+        if (pf==0){pf=(float)(salary*0.1);}
+        else{
+            pf=(float)(pf+salary*0.1);
+        }
+    }
+    @Override
+    public void cab(){
+        cab=true;
+    }
 }
 
 public class ProjectJava1 {
@@ -99,7 +155,7 @@ public class ProjectJava1 {
         Clerk c = null;
         do {
             System.out.println(
-                    "-----------------Available choices-----------------\n1. Create\n2. Display\n3. Raise\n4. Exit\n---------------------------------------------------\nEnter your choice: ");
+                    "-----------------Available choices-----------------\n1. Create\n2. Display\n3. Raise\n4. Update PF\n5. Exit\n---------------------------------------------------\nEnter your choice: ");
             choice2 = sc.nextInt();
             if (choice2 == 1) {
                 do {
@@ -201,12 +257,57 @@ public class ProjectJava1 {
                         }
                     }
                 } while (choice1 != 5);
-            } else if (choice2 == 4) {
+            }else if (choice2==4){
+                do {
+                    System.out.println(
+                            "-----------------Available choices-----------------\n1. Developer\n2. Clerk\n3. Manager\n4. Tester\n5. Exit");
+                    System.out.println("---------------------------------------------------\nEnter your choice: ");
+                    choice1 = sc.nextInt();
+                    if (choice1 == 1) {
+                        if (d != null) {
+                            Company cp=d;
+                            cp.pf();
+                            System.out.println("---------------------------------------------------");
+                        } else {
+                            System.out.println("---------------------------------------------------\n-------------!Please create the data!--------------\n---------------------------------------------------");
+                            choice1 = 5;
+                        }
+                    } else if (choice1 == 2) {
+                        if (c != null) {
+                            Company cp=c;
+                            cp.pf();
+                            System.out.println("---------------------------------------------------");
+                        } else {
+                            System.out.println("---------------------------------------------------\n-------------!Please create the data!--------------\n---------------------------------------------------");
+                            choice1 = 5;
+                        }
+                    } else if (choice1 == 3) {
+                        if (m != null) {
+                            Company cp=m;
+                            cp.pf();
+                            System.out.println("---------------------------------------------------");
+                        } else {
+                            System.out.println("---------------------------------------------------\n-------------!Please create the data!--------------\n---------------------------------------------------");
+                            choice1 = 5;
+                        }
+                    } else if (choice1 == 4) {
+                        if (t != null) {
+                            Company cp=t;
+                            cp.pf();
+                            System.out.println("---------------------------------------------------");
+                        } else {
+                            System.out.println("---------------------------------------------------\n-------------!Please create the data!--------------\n---------------------------------------------------");
+                            choice1 = 5;
+                        }
+                    }
+                } while (choice1 != 5);
+            } 
+            else if (choice2 == 5) {
                 System.out.println("---------------------------------------------------\nThank you!\n---------------------------------------------------");
                 System.exit(0);
             } else {
                 System.out.println("---------------------------------------------------\nInvalid Choice!!\n---------------------------------------------------");
             }
-        } while (choice2 != 4);
+        } while (choice2 != 5);
     }
 }
